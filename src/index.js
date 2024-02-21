fbca363a6fd05abe7ce623471ba1367304359262
legorythme <kanserthejash@gmail.com>
2024-02-21 16:00:06 +0100

    setup firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
