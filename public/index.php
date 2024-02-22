b6df689f91e30c29a132a84d1e950669554838fd
legorythme <kanserthejash@gmail.com>
2024-02-22 11:39:05 +0100

    adding switch case to index.php

--- public/index.php
+++ public/index.php
@@ -2,4 +2,17 @@
 
 // get the requested path
 $path = $_SERVER['REQUEST_URI'];
-die($path);
+
+switch ($path) {
+  case '/':
+    # TODO display the home page
+    die('home');
+    break;
+  case '/api/tickets':
+    # TODO display the tickets
+    die('tickets');
+    break;
+  default:
+    # TODO not found page
+    die('not found');
+}
